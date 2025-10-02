78dc6e80e39600e3199f0ebee6de2eea815bd8d1
Manan M <manan@theboco.in>
2025-10-02 18:27:44 -0400

    Update2 docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,19 +1,3 @@
-#!/bin/sh
-
-if [[ -z "$GID" ]]; then
-	GID="$UID"
-fi
-
-# Define functions.
-function fixperms {
-	chown -R $UID:$GID /data
-
-	# /opt/mautrix-whatsapp is read-only, so disable file logging if it's pointing there.
-	if [[ "$(yq e '.logging.writers[1].filename' /data/config.yaml)" == "./logs/mautrix-whatsapp.log" ]]; then
-		yq -I4 e -i 'del(.logging.writers[1])' /data/config.yaml
-	fi
-}
-
 # Generate config from environment variables if they exist and no config exists
 if [[ ! -f /data/config.yaml ]] && [[ -n "$HOMESERVER_DOMAIN" ]]; then
 	echo "Generating config.yaml from environment variables..."
@@ -35,49 +19,20 @@ appservice:
     bot_username: whatsappbot
     ephemeral_events: true
 
-bridge:
-    username_template: "whatsapp_{userid}"
+# MEGABRIDGE FORMAT (not legacy format)
+network:
     displayname_template: "{displayname} (WA)"
-    
+    username_template: "whatsapp_{userid}"
+
+bridge:
     permissions:
         "*": relay
         "${HOMESERVER_DOMAIN}": user
 
 logging:
-    version: 1
-    formatters:
-        colored:
-            (): mautrix.util.color_log.ColorFormatter
-    handlers:
-        console:
-            class: logging.StreamHandler
-            formatter: colored
-    loggers:
-        mau:
-            level: INFO
-    root:
-        level: INFO
-        handlers: [console]
+    min_level: info
+    writers:
+    - type: stdout
+      format: pretty-colored
 EOF
 fi
-
-if [[ ! -f /data/config.yaml ]]; then
-	/usr/bin/mautrix-whatsapp -c /data/config.yaml -e
-	echo "Didn't find a config file."
-	echo "Copied default config file to /data/config.yaml"
-	echo "Modify that config file to your liking."
-	echo "Start the container again after that to generate the registration file."
-	exit
-fi
-
-if [[ ! -f /data/registration.yaml ]]; then
-	/usr/bin/mautrix-whatsapp -g -c /data/config.yaml -r /data/registration.yaml || exit $?
-	echo "Didn't find a registration file."
-	echo "Generated one for you."
-	echo "See https://docs.mau.fi/bridges/general/registering-appservices.html on how to use it."
-	exit
-fi
-
-cd /data
-fixperms
-exec su-exec $UID:$GID /usr/bin/mautrix-whatsapp
