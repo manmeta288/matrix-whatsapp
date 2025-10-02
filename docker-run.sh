5ef6c00c769dc90247021c06ed58bdf6b539aeb6
Manan M <manan@theboco.in>
2025-10-02 18:30:15 -0400

    Update2 docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,19 @@
+#!/bin/sh
+
+if [[ -z "$GID" ]]; then
+	GID="$UID"
+fi
+
+# Define functions.
+function fixperms {
+	chown -R $UID:$GID /data
+
+	# /opt/mautrix-whatsapp is read-only, so disable file logging if it's pointing there.
+	if [[ "$(yq e '.logging.writers[1].filename' /data/config.yaml)" == "./logs/mautrix-whatsapp.log" ]]; then
+		yq -I4 e -i 'del(.logging.writers[1])' /data/config.yaml
+	fi
+}
+
 # Generate config from environment variables if they exist and no config exists
 if [[ ! -f /data/config.yaml ]] && [[ -n "$HOMESERVER_DOMAIN" ]]; then
 	echo "Generating config.yaml from environment variables..."
@@ -19,7 +35,7 @@ appservice:
     bot_username: whatsappbot
     ephemeral_events: true
 
-# MEGABRIDGE FORMAT (not legacy format)
+# NEW MEGABRIDGE FORMAT
 network:
     displayname_template: "{displayname} (WA)"
     username_template: "whatsapp_{userid}"
@@ -36,3 +52,24 @@ logging:
       format: pretty-colored
 EOF
 fi
+
+if [[ ! -f /data/config.yaml ]]; then
+	/usr/bin/mautrix-whatsapp -c /data/config.yaml -e
+	echo "Didn't find a config file."
+	echo "Copied default config file to /data/config.yaml"
+	echo "Modify that config file to your liking."
+	echo "Start the container again after that to generate the registration file."
+	exit
+fi
+
+if [[ ! -f /data/registration.yaml ]]; then
+	/usr/bin/mautrix-whatsapp -g -c /data/config.yaml -r /data/registration.yaml || exit $?
+	echo "Didn't find a registration file."
+	echo "Generated one for you."
+	echo "See https://docs.mau.fi/bridges/general/registering-appservices.html on how to use it."
+	exit
+fi
+
+cd /data
+fixperms
+exec su-exec $UID:$GID /usr/bin/mautrix-whatsapp
